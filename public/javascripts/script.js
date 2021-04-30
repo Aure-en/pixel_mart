b3e342616e9a95a059b5952b43b9f6a7d67ecc5a
Aureen <yukiansff@gmail.com>
2021-04-30 15:29:08 +0200

    Improved style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,35 @@
 // Nav
 
 window.addEventListener('load', () => {
-  let isDropdownOpen = false;
+  let isDropdownOpen;
   const navBtn = document.querySelector('.navBtn');
   const navDropdown = document.querySelector('.navDropdown');
-  navBtn.addEventListener('click', () => {
-    if (isDropdownOpen) {
-      navDropdown.classList.add('navDropdown--active');
+
+  const initialNav = () => {
+    if (window.innerWidth < 768) {
+      navDropdown.classList.remove('navDropdown--active');
+      navBtn.classList.remove('hidden');
       isDropdownOpen = false;
     } else {
+      navDropdown.classList.add('navDropdown--active');
+      navBtn.classList.add('hidden');
+      isDropdownOpen = true;
+    }
+  };
+
+  initialNav();
+
+  navBtn.addEventListener('click', () => {
+    if (isDropdownOpen) {
       navDropdown.classList.remove('navDropdown--active');
+      isDropdownOpen = false;
+    } else {
+      navDropdown.classList.add('navDropdown--active');
       isDropdownOpen = true;
     }
   });
+
+  window.addEventListener('resize', initialNav);
 });
 
 // Preview
